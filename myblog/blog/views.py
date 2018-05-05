eef34cdaf0ed5f87805b5bb70e2f31059f1fda61
hi-dove <452133098@qq.com>
2018-05-05 22:47:06 +0800

    我的博客

---
+++ myblog/blog/views.py
@@ -0,0 +1,72 @@
+import markdown
+from django.shortcuts import render, get_object_or_404
+from comments.forms import CommentForm
+from . models import Post, Category, Tag
+from django.views.generic import ListView
+
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    contexr_object_name  = 'post_list'
+    paginate_by = 5
+
+
+def aboutme(request):
+    return render(request,'aboutme.html')
+
+
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    #阅读量统计：detail视图函数每被调用一次，阅读量+1
+    post.increase_views()
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    form = CommentForm()
+    #获取这篇post下的全部评论
+    comment_list = post.comment_set.all()
+    # 将文章、表单、以及文章下的评论列表作为模板变量传给 detail.html 模板，以便渲染相应数据。
+    context = {'post':post,
+               'form':form,
+               'comment_list': comment_list}
+    return render(request, 'blog/detail.html', context=context)
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk=self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
+
+
+from django.db.models import Q
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
+
+    if not q:
+        error_msg = "请输入关键词"
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'error_msg': error_msg,
+                                               'post_list': post_list})
